f007d0de4bc423d5abfe3061624837c1ca8b8e84
lu-lay <dmsal547@gmail.com>
2020-08-26 17:23:15 +0900

    Chagne variable name to main kt (rnds -> first_rnds)

--- app/src/main/java/ko/co/montyhall3doors/MainActivity.kt
+++ app/src/main/java/ko/co/montyhall3doors/MainActivity.kt
@@ -1,7 +1,6 @@
 package ko.co.montyhall3doors
 
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
@@ -16,20 +15,22 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 
-        val rnds = (0..2).random()
-        print("$rnds" + "입니다")
-        Log.e("for 문의 결과는", "$rnds" + "입니다")
+        val first_rnds = (0..2).random()
+        print("$first_rnds" + "입니다")
 
-        if (rnds == 0) {
+        if (first_rnds == 0) {
             img_door1_back.setImageResource(R.drawable.supercar_64)
             img_door1_back.visibility = View.GONE
 
-        } else if (rnds == 1) {
+        } else if (first_rnds == 1) {
             img_door2_back.setImageResource(R.drawable.supercar_64)
             img_door2_back.visibility = View.GONE
         } else {
             img_door3_back.setImageResource(R.drawable.supercar_64)
             img_door3_back.visibility = View.GONE
+
+
+         
 
 
         }
