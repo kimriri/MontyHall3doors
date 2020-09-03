3eb7a5216c8829ead1a2fb54f49792f6478b7077
lu-lay <dmsal547@gmail.com>
2020-09-04 06:06:05 +0900

    Modifind naming Convention  to Main kt

--- app/src/main/java/ko/co/montyhall3doors/MainActivity.kt
+++ app/src/main/java/ko/co/montyhall3doors/MainActivity.kt
@@ -18,52 +18,51 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
 
 
-    var sum: Long = 0
-    var cont: Long = 0
-    var percentage: Double = 0.0
-    var rnds_start = 0
-    var Select = 0
-    var finalSelect = 0
-    var SupCar = 0
+    var MainSum: Long = 0
+    var MainCont: Long = 0
+    var MainPercentage: Double = 0.0
+    var MainRandomStart = 0
+    var MainSelect = 0
+    var MainFinalSelect = 0
+    var MainSupCar = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val btn_event = BtnEvent();
-        btn_next.setOnClickListener(btn_event);
-        btn_restart.setOnClickListener(btn_event);
-        btn_view_results.setOnClickListener(btn_event)
-        Start()
-        rnds()
+        val btnMainEvent = BtnEvent();
+        btn_next.setOnClickListener(btnMainEvent);
+        btn_restart.setOnClickListener(btnMainEvent);
+        btn_view_results.setOnClickListener(btnMainEvent)
+        initializeView()
+        RandomNumberGenerator()
 
     }
 
 
-    fun Start() {
+    private fun initializeView() {
 
         img_door0.visibility = View.VISIBLE
         img_door1.visibility = View.VISIBLE
-        img_door2.visibility = View.VISIBLE
         btn_next.visibility = View.VISIBLE
         btn_view_results.visibility = View.GONE
         btn_restart.visibility = View.GONE
         iv_selection0.visibility = View.GONE
         iv_selection1.visibility = View.GONE
         iv_selection2.visibility = View.GONE
-        rnds()
+        RandomNumberGenerator()
     }
 
-    fun rnds() {
-        val first_rnds = (0..2).random()
+    private fun RandomNumberGenerator() {
+        val MainFirstRandom = (0..2).random()
 
-        rnds_start = first_rnds
-        Start_rnds()
-        Select_doors()
+        MainRandomStart = MainFirstRandom
+        MainInputRandom()
+        MainSelectDoors()
     }
 
-    fun Start_rnds() {
+    private fun MainInputRandom() {
 
-        when (rnds_start) {
+        when (MainRandomStart) {
 
             0 -> {
                 img_door0_back.setImageResource(R.drawable.supercar_64)
@@ -71,7 +70,7 @@ class MainActivity : AppCompatActivity() {
                 img_door2_back.setImageResource(R.drawable.goat_64)
                 img_door0_back.visibility = View.VISIBLE
 
-                SupCar = 0
+                MainSupCar = 0
 
             }
             1 -> {
@@ -80,21 +79,21 @@ class MainActivity : AppCompatActivity() {
                 img_door2_back.setImageResource(R.drawable.goat_64)
                 img_door1_back.visibility = View.VISIBLE
 
-                SupCar = 1
+                MainSupCar = 1
             }
             else -> {
                 img_door2_back.setImageResource(R.drawable.supercar_64)
                 img_door1_back.setImageResource(R.drawable.goat_64)
                 img_door0_back.setImageResource(R.drawable.goat_64)
                 img_door2_back.visibility = View.VISIBLE
-                SupCar = 2
+                MainSupCar = 2
 
             }
 
         }
     }
 
-    fun Select_doors() {
+    private fun MainSelectDoors() {
 
         img_door0.setOnClickListener {
             iv_selection0.visibility = View.VISIBLE
@@ -116,20 +115,20 @@ class MainActivity : AppCompatActivity() {
 
 
     @SuppressLint("ShowToast", "SetTextI18n")
-    private fun Popup() {
+    private fun MainPopup() {
 
         val inflater = getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
         val view = inflater.inflate(R.layout.main_popup, null)
-        val textView: TextView = view.findViewById(R.id.main_popup)
-        val Percent = String.format("%.2f", percentage)
+        val TVMainPopup: TextView = view.findViewById(R.id.main_popup)
+        val MainPopupPercent = String.format("%.2f", MainPercentage)
 
-        Log.e("확률은", "$Percent 입니다.")
+        Log.e("확률은", "$MainPopupPercent 입니다.")
 
-        textView.text = "총 $cont 번 시도 하여서 약 $Percent 의 확률을 얻었습니다."
+        TVMainPopup.text = "총 $MainCont 번 시도하여 약 $MainPopupPercent 의 확률을 얻었습니다."
         val alertDialog = AlertDialog.Builder(this)
             .setTitle("결과")
             .setPositiveButton("보기") { dialog, which ->
-                Toast.makeText(applicationContext, "결과보여주기", Toast.LENGTH_SHORT)
+                Toast.makeText(applicationContext, "결과 보여주기", Toast.LENGTH_SHORT)
             }
             .setNegativeButton("취소", null)
             .create()
@@ -138,9 +137,6 @@ class MainActivity : AppCompatActivity() {
         alertDialog.show()
     }
 
-
-    //   버튼 이벤트 -  > 바꿧는지 바꾸지 않았는지  확인 하기
-    // 바꿧다면 바꿧을 때의 확률만 계산하기
 
     inner class BtnEvent : View.OnClickListener {
         @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
@@ -165,7 +161,7 @@ class MainActivity : AppCompatActivity() {
                             when {
                                 iv_selection0.visibility.equals(View.VISIBLE) -> {
                                     when {
-                                        rnds_start.equals(0) -> {  // 0번에 차가 있을 경우
+                                        MainRandomStart.equals(0) -> {
 
                                             val Second_rnds = intArrayOf(1, 2).random()
                                             when (Second_rnds) {
@@ -177,10 +173,10 @@ class MainActivity : AppCompatActivity() {
                                                 }
                                             }
                                         }
-                                        rnds_start.equals(1) -> {
+                                        MainRandomStart.equals(1) -> {
                                             img_door2.visibility = View.INVISIBLE
                                         }
-                                        rnds_start.equals(2) -> {
+                                        MainRandomStart.equals(2) -> {
                                             img_door1.visibility = View.INVISIBLE
 
                                         }
@@ -191,7 +187,7 @@ class MainActivity : AppCompatActivity() {
 
                                 iv_selection1.visibility.equals(View.VISIBLE) -> {
                                     when {
-                                        rnds_start.equals(1) -> {
+                                        MainRandomStart.equals(1) -> {
 
                                             val Second_rnds = intArrayOf(0, 2).random()
                                             when (Second_rnds) {
@@ -203,10 +199,10 @@ class MainActivity : AppCompatActivity() {
                                                 }
                                             }
                                         }
-                                        rnds_start.equals(0) -> {
+                                        MainRandomStart.equals(0) -> {
                                             img_door2.visibility = View.INVISIBLE
                                         }
-                                        rnds_start.equals(2) -> {
+                                        MainRandomStart.equals(2) -> {
                                             img_door0.visibility = View.INVISIBLE
 
                                         }
@@ -215,7 +211,7 @@ class MainActivity : AppCompatActivity() {
                                 }
                                 iv_selection2.visibility.equals(View.VISIBLE) -> {
                                     when {
-                                        rnds_start.equals(2) -> {
+                                        MainRandomStart.equals(2) -> {
 
                                             val Second_rnds = intArrayOf(0, 1).random()
                                             when (Second_rnds) {
@@ -227,10 +223,10 @@ class MainActivity : AppCompatActivity() {
                                                 }
                                             }
                                         }
-                                        rnds_start.equals(0) -> {
+                                        MainRandomStart.equals(0) -> {
                                             img_door1.visibility = View.INVISIBLE
                                         }
-                                        rnds_start.equals(1) -> {
+                                        MainRandomStart.equals(1) -> {
                                             img_door0.visibility = View.INVISIBLE
                                         }
                                     }
@@ -240,19 +236,19 @@ class MainActivity : AppCompatActivity() {
 
                             when (View.VISIBLE) {
                                 iv_selection0.visibility -> {
-                                    Select = 0
+                                    MainSelect = 0
                                 }
                                 iv_selection1.visibility -> {
 
-                                    Select = 1
+                                    MainSelect = 1
                                 }
                                 iv_selection2.visibility -> {
 
-                                    Select = 2
+                                    MainSelect = 2
                                 }
 
                             }
-                            Log.e("파이널", "두번째 질문에서의 Select 는 $Select 입니다")
+                            Log.e("파이널", "두번째 질문에서의 Select 는 $MainSelect 입니다")
                         } else if (tv_main_question1.visibility == View.VISIBLE) {
 
 
@@ -269,67 +265,67 @@ class MainActivity : AppCompatActivity() {
 
                             when (View.VISIBLE) {
                                 iv_selection0.visibility -> {
-                                    finalSelect = 0
+                                    MainFinalSelect = 0
                                 }
                                 iv_selection1.visibility -> {
 
-                                    finalSelect = 1
+                                    MainFinalSelect = 1
                                 }
                                 iv_selection2.visibility -> {
 
-                                    finalSelect = 2
+                                    MainFinalSelect = 2
 
                                 }
 
                             }
-                            Log.e("파이널", "finalSelect는 $finalSelect 입니다  ")
-                            if (iv_selection0.visibility == View.VISIBLE && SupCar == 0) {
-                                cont++
-                                if (Select != finalSelect) {
+                            Log.e("파이널", "finalSelect 는 $MainFinalSelect 입니다  ")
+                            if (iv_selection0.visibility == View.VISIBLE && MainSupCar == 0) {
+                                MainCont++
+                                if (MainSelect != MainFinalSelect) {
 
-                                    Log.e("파이널", "Select 는 $Select 입니다")
-                                    Log.e("파이널", "sum 는 $sum 입니다")
-                                    Log.e("파이널", "SupCar 는 $SupCar 입니다")
+                                    Log.e("파이널", "Select 는 $MainSelect 입니다")
+                                    Log.e("파이널", "sum 는 $MainSum 입니다")
+                                    Log.e("파이널", "SupCar 는 $MainSupCar 입니다")
                                     Log.e(
                                         "파이널",
-                                        "Select 와 finalSelect 는 $Select 와 $finalSelect 입니다"
+                                        "Select 와 finalSelect 는 $MainSelect 와 $MainFinalSelect 입니다"
                                     )
-                                    sum++
+                                    MainSum++
                                 }
-                                Log.e("sum", "$sum 개")
-                            } else if (iv_selection1.visibility == View.VISIBLE && SupCar == 1) {
-                                cont++
-                                if (Select != finalSelect) {
+                                Log.e("sum", "$MainSum 개")
+                            } else if (iv_selection1.visibility == View.VISIBLE && MainSupCar == 1) {
+                                MainCont++
+                                if (MainSelect != MainFinalSelect) {
 
-                                    sum++
+                                    MainSum++
 
-                                    Log.e("파이널", "Select 는 $Select 입니다")
-                                    Log.e("파이널", "sum 는 $sum 입니다")
-                                    Log.e("파이널", "SupCar 는 $SupCar 입니다")
+                                    Log.e("파이널", "Select 는 $MainSelect 입니다")
+                                    Log.e("파이널", "sum 는 $MainSum 입니다")
+                                    Log.e("파이널", "SupCar 는 $MainSupCar 입니다")
                                     Log.e(
                                         "파이널",
-                                        "Select 와 finalSelect 는 $Select 와 $finalSelect 입니다"
+                                        "Select 와 finalSelect 는 $MainSelect 와 $MainFinalSelect 입니다"
                                     )
                                 }
-                                Log.e("sum", "$sum 개")
-                            } else if (iv_selection2.visibility == View.VISIBLE && SupCar == 2) {
-                                cont++
-                                if (Select != finalSelect) {
-                                    sum++
-                                    Log.e("파이널", "Select 는 $Select 입니다")
-                                    Log.e("파이널", "sum 는 $sum 입니다")
-                                    Log.e("파이널", "SupCar 는 $SupCar 입니다")
+                                Log.e("sum", "$MainSum 개")
+                            } else if (iv_selection2.visibility == View.VISIBLE && MainSupCar == 2) {
+                                MainCont++
+                                if (MainSelect != MainFinalSelect) {
+                                    MainSum++
+                                    Log.e("파이널", "Select 는 $MainSelect 입니다")
+                                    Log.e("파이널", "sum 는 $MainSum 입니다")
+                                    Log.e("파이널", "SupCar 는 $MainSupCar 입니다")
                                     Log.e(
                                         "파이널",
-                                        "Select 와 finalSelect 는 $Select 와 $finalSelect 입니다"
+                                        "Select 와 finalSelect 는 $MainSelect 와 $MainFinalSelect 입니다"
                                     )
                                 }
                             } else {
-                                cont++
-                                Log.e("파이널", "모두 아닐때 sum 는 $sum 입니다")
+                                MainCont++
+                                Log.e("파이널", "모두 아닐때 sum 는 $MainSum 입니다")
 
                             }
-                            Log.e("count", "$cont 개")
+                            Log.e("count", "$MainCont 개")
                         }
                     }
 
@@ -337,16 +333,18 @@ class MainActivity : AppCompatActivity() {
                 R.id.btn_restart -> {
                     tv_main_question2.visibility = View.INVISIBLE
                     tv_main_question0.visibility = View.VISIBLE
-                    Start()
+                    initializeView()
                 }
 
                 R.id.btn_view_results -> {
 
-                    percentage = (sum.toDouble() / cont.toDouble()) * 100
+                    MainPercentage = (MainSum.toDouble() / MainCont.toDouble()) * 100
 
-                    Log.e("확률은", " sum 은  $sum 이고  cont는 $cont 입니다.  $percentage 입니다.")
-
-                    Popup()
+                    Log.e(
+                        "확률은",
+                        " MainSum 은  $MainSum 이고 MainCont 는 $MainCont, MainPercentage 은 $MainPercentage 입니다."
+                    )
+                    MainPopup()
                 }
             }
         }
